a71dbdc8411c3815de446a95f3e1c5bd6637b884
Kenny <keacolli@ucsc.edu>
2020-06-29 16:46:36 -0700

    implement auth redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { v4 as uuid } from 'uuid';
 
-export default () => (
-  <div style={{ textAlign: 'center' }}>
-    <h1>Welcome to React-Static</h1>
-  </div>
-)
+export default () => {
+  const authorize = () => {
+    const url = 'https://accounts.spotify.com/authorize';
+    const clientId = 'c034101426734eb990996c5358c9a0b6';
+    const redirectUri = 'http://localhost:3000/callback';
+    const scopes = ['user-read-recently-played'];
+
+    const state = uuid();
+    localStorage.setItem('stateKey', state);
+
+    return `${url}?response_type=token&client_id=${clientId}&scope=${scopes}&redirect_uri=${redirectUri}&state=${state}`;
+  };
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Welcome to Spotify Genius</h1>
+      <a href={authorize()}>
+        <button>Log in to Spotify</button>
+      </a>
+    </div>
+  );
+};
---
+++ src/pages/lyrics.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <p>
+      React Static is adsafdasfrdsa progressive static site generator for React.
+    </p>
+  </div>
+);
