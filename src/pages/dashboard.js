44b178572aa60a0254d57da280bc9cdd1b6df85e
dw-ll <danswilliams95@gmail.com>
2020-07-14 20:05:04 -0700

    Add term switch button.

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -21,34 +21,39 @@ const Frame = styled.div`
     flex 
     mx-12
     py-10
+    bg-black
+    h-screen
     justify-around
   `}
 
 `;
 
+const TermButton = styled.div`
+${tw`bg-black border border-green-500 hover:bg-green-500 text-green-500 hover:text-black font-bold py-2 px-4 rounded-md shadow-md mx-4 cursor-pointer`}`;
+
 const HeaderText = styled.div`
 ${tw`
   text-3xl
   font-bold
+  text-green-500
 
 `}`;
 
-const ItemText = styled.div`
-${tw`
-  text-lg
-  font-semibold
-`}`;
-const ListContainer = styled.div`
-${tw`
-  px-2
-`}`;
 export default () => {
 
-  const key = 'BQC1cywahDxID4Za6iNf23nBzMNFwhXmL7wvtakcVqhd7jq2Ts5M5pj2cSgCLykno2sJgJHFVD4LR6jI86dQZQ34C0t1Jz6H-OOkHTimi3kAWFcbDWFs7xtwkVU319YaAlVdXO7X4iIxCU3qd8e4NqJy_XA'
-  const [userTaste, setUserTaste] = useState({ tracks: [], artists: [] })
 
+  /* State */
+  const key = 'BQDrlvEXL4deOnCQHtb6rUiMO2VFjlO7RPR3cRgcgX5p8CF4_01l15LazejXPXfgkHEC37R_DXd7Pf6aByMCsIlYJa0UVTthzPAqFGwPulwPSRMqGifsMxGw-nOmH7WfVPF5obDRehcI6gbZ7AKLXYkDtJM'
+  const [userTaste, setUserTaste] = useState({ tracks: [], artists: [] })
+  const [term, setTerm] = useState('medium')
+
+  const handleClick = e => {
+    setTerm(e.target.id)
+  }
+
+  /* Calls Spotify Taste Endpoint */
   const getUserTaste = async (accessToken, tasteType) => {
-    const res = await axios.get(`https://api.spotify.com/v1/me/top/${tasteType}`,
+    const res = await axios.get(`https://api.spotify.com/v1/me/top/${tasteType}?time_range=${term}_term&limit=10`,
       { headers: { "Authorization": `Bearer ${accessToken}` } })
     console.log(res.data.items)
     console.log(tasteType)
@@ -58,16 +63,28 @@ export default () => {
   React.useEffect(() => {
     getUserTaste(key, 'tracks');
     getUserTaste(key, 'artists');
-  }, []);
+  }, [term]);
 
-  return (
+  return (<div class='bg-black' >
+    <div class="flex justify-center">
+      <TermButton id='long' class="bg-black border border-green-500 hover:bg-green-500 text-green-500 hover:text-black font-bold py-2 px-4 rounded-md shadow-md mx-4 cursor-pointer" onClick={handleClick}>
+        All-Time
+      </TermButton>
+      <TermButton id='medium' class="bg-black border border-green-500 hover:bg-green-500 text-green-500 hover:text-black font-bold py-2 px-4 rounded-md shadow-md mx-4 cursor-pointer" onClick={handleClick}>
+        This Year
+      </TermButton>
+      <TermButton id='short' class="bg-black border border-green-500 hover:bg-green-500 text-green-500 hover:text-black font-bold py-2 px-4 rounded-md shadow-md mx-4 cursor-pointer" onClick={handleClick}>
+        This Month
+      </TermButton>
+    </div>
+
     <Frame>
       <div>
         <HeaderText>Your Favorite Artists</HeaderText>
         <ul className='h-full list-disc ml-3'>
           {console.log(userTaste)}
           {userTaste.artists.map(item => {
-            return <li className='py-2'>{item.name} </li>
+            return <li className='py-2 text-green-500'>{item.name} </li>
           })}
         </ul>
       </div>
@@ -76,13 +93,14 @@ export default () => {
         <HeaderText>Your Favorite Songs</HeaderText>
         <ul className='h-full list-disc ml-3'>
           {userTaste.tracks.map((item) => {
-            return <li className='py-2' >{item.album.artists[0].name} - {item.name} </li>
+            return <li className='py-2 text-green-500' >{item.album.artists[0].name} - {item.name} </li>
           })}
         </ul>
       </div>
 
 
     </Frame>
+  </div>
 
   )
 };
