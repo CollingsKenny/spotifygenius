c5529db4e246d2c4bc3389da6d6adbbff4daefe3
dw-ll <danswilliams95@gmail.com>
2020-07-16 15:17:27 -0700

    Add dashboard frame, colors, icons.

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -2,33 +2,58 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import tw from 'tailwind.macro';
 import styled from '@emotion/styled';
-import { accessKey } from '../config';
+import { Icon, InlineIcon } from '@iconify/react';
+import heartIcon from '@iconify/icons-heroicons-outline/heart';
 
+
+import { accessKey } from '../config';
 import SongList from '../containers/songList';
 import ArtistList from '../containers/artistList';
 import FancyButton from '../components/fancyButton';
 
-const Container = styled.div`
-  ${tw`
-    bg-black
-  `}
+
+const Container = styled.main`
+  display:flex;
+  width:100%;
+  height:900px;
+  background:#222222;
 `;
-const TermNav = styled.nav`
-  ${tw`
-  flex justify-center
-  `}
+const DashboardFrame = styled.div`
+  display:flex;
+  width:100%;
+  height:685px;
+  margin: 2rem 4rem;
+  background: #3A3A3A;
+  box-shadow: 0px 4px 4px rgba(0,0,0,0.25);
+  border-radius:50px;
 `;
 
-const Frame = styled.div`
-  ${tw`
-    flex 
-    mx-12
-    py-10
-    bg-black
-    h-screen
-    justify-around
-  `}
+const NavSidebar = styled.aside`
+  display:flex;
+  flex-direction:column; 
+  align-items:center;
+  width:135px;
+  height:685px;
+  background:#1DB954;
+  box-shadow: 2px 0px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 50px 0px 0px 50px;
 `;
+const SideBarDivider = styled.div`
+  width:135px;
+  border: 1px solid #3A3A3A;
+`;
+
+const IconContainer = styled.div`
+  margin:1rem 1rem;
+`;
+
+const UserProfilePhoto = styled.div`
+  width:53px;
+  height:53px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 259.5px;
+`;
+
 
 export default () => {
   /* Calls Spotify Taste Endpoint */
@@ -90,22 +115,39 @@ export default () => {
 
   return (
     <Container>
-      <TermNav>
-        <FancyButton id='long' onClick={termClick} active={term === 'long'}>
-          All-Time
-        </FancyButton>
-        <FancyButton id='medium' onClick={termClick} active={term === 'medium'}>
-          This Year
-        </FancyButton>
-        <FancyButton id='short' onClick={termClick} active={term === 'short'}>
-          This Month
-        </FancyButton>
-      </TermNav>
+      <DashboardFrame>
+        <NavSidebar>
+          <IconContainer>
+            <UserProfilePhoto src='https://res.cloudinary.com/dtu8zsq1c/image/upload/v1594601133/portfolio-assets/download20200700162348_b4rglf.png'></UserProfilePhoto>
+          </IconContainer>
+          <SideBarDivider />
+          <IconContainer>
+            <Icon width="35" height="40" color="#3A3A3A" icon={heartIcon} />
+          </IconContainer>
+          <SideBarDivider />
+          <IconContainer>
+            <Icon width="35" height="40" color="#3A3A3A" icon={heartIcon} />
+          </IconContainer>
+          <SideBarDivider />
+          <IconContainer>
+            <Icon width="35" height="40" color="#3A3A3A" icon={heartIcon} />
+          </IconContainer>
+          <SideBarDivider />
+          <IconContainer>
+            <Icon width="35" height="40" color="#3A3A3A" icon={heartIcon} />
+          </IconContainer>
+          <SideBarDivider />
+          <IconContainer>
+            <Icon width="35" height="40" color="#3A3A3A" icon={heartIcon} />
+          </IconContainer>
+          <SideBarDivider />
+          <IconContainer>
+            <Icon width="35" height="40" color="#3A3A3A" icon={heartIcon} />
+          </IconContainer>
+          <SideBarDivider />
 
-      <Frame>
-        <ArtistList artists={userTaste.artists} />
-        <SongList tracks={userTaste.tracks} features={tracksFeatures} />
-      </Frame>
+        </NavSidebar>
+      </DashboardFrame>
     </Container>
   );
 };
